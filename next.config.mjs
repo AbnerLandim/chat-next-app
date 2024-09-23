4d23a711db452850c65ac401230ebcf03ccf076e
AbnerLandim <abner.landim340@gmail.com>
2024-09-23 14:19:58 -0300

    chore: import dirname from path

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /** @type {import('next').NextConfig} */
-import path from 'path';
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
   webpack: (config, { isServer }) => {
-    config.resolve.alias['@'] = path.join(__dirname, '/');
+    config.resolve.alias["@"] = path.join(__dirname);
     return config;
   },
 };
